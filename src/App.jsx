dadf247b91db359c8b5f87a281e2b6042260bbf4
ramesh12331 <mamidiramesh503@gmail.com>
2025-07-09 01:04:42 +0530

    About page created

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/Header";
+import About from "./components/About";
+
+
+const App = () => {
+  return (
+    <div className="w-full overflow-hidden">
+      <Header/>
+      <About/>
+    </div>
+  );
+};
+
+export default App;
