78dd42a8c19d6d92cf5e384755c1094904c152b4
ramesh12331 <mamidiramesh503@gmail.com>
2025-07-09 20:45:59 +0530

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Testimonials from "./components/Testimonials";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
       <Projects/>
       <Testimonials/>
       <Contact/>
+      <Footer/>
     </div>
   );
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const Footer = () => {
+  return (
+    <div className="pt-10 px-4 md:px-20 lg:px-32 bg-gray-900 w-full overflow-hidden">
+      <div className="mx-auto flex flex-col md:flex-row">
+        <div className="w-full md:w-1/3 mb-8 mb:mb-0">
+          <img src={assets.logo_dark} alt="" />
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text.
+          </p>
+        </div>
+        <div></div>
+        <div></div>
+      </div>
+      {/*  */}
+      <div></div>
+    </div>
+  );
+};
+
+export default Footer;
