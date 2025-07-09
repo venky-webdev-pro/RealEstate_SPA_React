b536ea0efab6cf439e86a62bdbef0285da7a83cf
ramesh12331 <mamidiramesh503@gmail.com>
2025-07-09 12:31:58 +0530

    project Section update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
 import About from "./components/About";
+import Projects from "./components/Projects";
 
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <div className="w-full overflow-hidden">
       <Header/>
       <About/>
+      <Projects/>
     </div>
   );
 };
