6e1362c52ec7ca28f0e579337b6bbd4839335cec
venky-webdev-pro <Nimmanavenkatalakshmi93@gmail.com>
2025-07-10 22:11:35 +0530

    About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      About
+    <div  className='flex items-center justify-center flex-col  mx-auto p-14 md:px-20 lg:px-32  w-full overflow-hidden'>
+      <h1 className='text-2xl font-bold mb-2'> About<span className=' underline underline-offset-4   decoration-1 font-light'>Our Brand</span></h1>
+      <p className='text-gray-500  text-center mb-6 max-w-80'>Passionate About Properties, Dedicated to Your Vision </p>
     </div>
   )
 }
 
-export default About
+export default About;
