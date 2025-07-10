958471d09ce6efbfbd758a8dd3577f7179f14a06
ramesh12331 <mamidiramesh503@gmail.com>
2025-07-10 16:05:33 +0530

    final

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,19 +4,44 @@ import { assets } from "../assets/assets";
 const Footer = () => {
   return (
     <div className="pt-10 px-4 md:px-20 lg:px-32 bg-gray-900 w-full overflow-hidden">
-      <div className="mx-auto flex flex-col md:flex-row">
+      <div className="mx-auto flex flex-col  md:flex-row justify-between">
         <div className="w-full md:w-1/3 mb-8 mb:mb-0">
           <img src={assets.logo_dark} alt="" />
-          <p>
+          <p className="text-gray-400 mt-4">
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text.
           </p>
         </div>
-        <div className="text-white">abc</div>
-        <div></div>
+        <div className="w-full md:w-1/5 mb-8 md:mb-0">
+          <h3 className="text-white text-lg font-bold mb-4">Company</h3>
+          <ul className="text-gray-400">
+            <li className="hover:text-white">Home</li>
+            <li className="hover:text-white">About us</li>
+            <li className="hover:text-white">Contact us</li>
+            <li className="hover:text-white">Privacy policy</li>
+          </ul>
+        </div>
+        <div className="w-full md:w-1/3">
+          <h3 className="text-white text-lg font-bold mb-4">
+            Subscribe to our newsletter
+          </h3>
+          <p className="text-gray-400 mb-4 max-w-80">
+            The latest news, articles, and resources, sent to your inbox weekly.
+          </p>
+          <div className="flex gap-2">
+            <input
+              type="email"
+              placeholder="Enter Your Email"
+              className="p-2 rounded bg-gray-800 text-gray-400 border border-gray-700 focus:outline-none w-full md:w-auto"
+            />
+            <button className="py-2 px-4 rounded bg-blue-500 text-white">Subscribe</button>
+          </div>
+        </div>
       </div>
       {/*  */}
-      <div></div>
+      <div className="border-t border-gray-700 py-4 mt-10 text-center text-gray-500">
+        Copyright 2024 © GreatStack. All Right Reserved
+      </div>
     </div>
   );
 };
