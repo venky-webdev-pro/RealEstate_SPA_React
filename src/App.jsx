5a30b2f96d062c41d8f23731ead0746f250fbf75
venky-webdev-pro <Nimmanavenkatalakshmi93@gmail.com>
2025-07-10 20:54:20 +0530

    realEstate

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
 import About from "./components/About";
-import Projects from "./components/Projects";
-import Testimonials from "./components/Testimonials";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-import { ToastContainer } from 'react-toastify';
+
 
 const App = () => {
   return (
     <div className="w-full overflow-hidden">
-      <ToastContainer/>
-      <Header/>
-      <About/>
-      <Projects/>
-      <Testimonials/>
-      <Contact/>
-      <Footer/>
+      <Header />
+      <About />
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { assets } from "../assets/assets";
 
 const Navbar = () => {
-  const [shoMobileMenu,setShowMobileMenu] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
-  useEffect(()=>{
-    if(shoMobileMenu){
-      document.body.style.overflow='hidden'
-    }else{
-      document.body.style.overflow='auto'
-    }
-
-    return ()=>{
-      document.body.style.overflow='auto'
-    }
-  },[shoMobileMenu])
   return (
     <div className="absolute top-0 left-0 w-full z-10">
       <div className="flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent">
@@ -29,20 +18,25 @@ const Navbar = () => {
         <button className="hidden md:block bg-white px-8 py-2 rounded-full">
           Sign up
         </button>
-        <img onClick={()=>setShowMobileMenu(true)} src={assets.menu_icon} className="md:hidden w-7" alt="" />
+        <div className="md:hidden" onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? (
+            <img src={assets.cross_icon} alt="" className="w-6" />
+          ) : (
+            <img src={assets.menu_icon} className="w-7" alt="" />
+          )}
+        </div>
       </div>
       {/* ------------------ Mobile-menu --------------------- */}
-      <div className={`md:hidden ${shoMobileMenu ? "fixed w-full":"h-0 w-0"}   right-0 top-0 bottom-0 overflow-hidden bg-white transition-all`}>
-        <div className="flex justify-end p-6 cursor-pointer">
-          <img onClick={()=>setShowMobileMenu(false)} src={assets.cross_icon} alt=""  className="w-6"/>
+      {isOpen && (
+        <div className="md:hidden fixed w-full  right-0 top-16 bottom-0 overflow-hidden bg-white transition-all">
+          <ul className="flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium">
+            <li onClick={()=>setIsOpen(false)} className="cursor-pointer hover:text-gray-400">Home</li>
+            <li onClick={()=>setIsOpen(false)} className="cursor-pointer hover:text-gray-400">About</li>
+            <li onClick={()=>setIsOpen(false)} className="cursor-pointer hover:text-gray-400">Projects</li>
+            <li onClick={()=>setIsOpen(false)} className="cursor-pointer hover:text-gray-400">Testimonials</li>
+          </ul>
         </div>
-        <ul className="flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium">
-          <li onClick={()=>setShowMobileMenu(false)} className="cursor-pointer hover:text-gray-400">Home</li>
-          <li onClick={()=>setShowMobileMenu(false)} className="cursor-pointer hover:text-gray-400">About</li>
-          <li onClick={()=>setShowMobileMenu(false)} className="cursor-pointer hover:text-gray-400">Projects</li>
-          <li onClick={()=>setShowMobileMenu(false)} className="cursor-pointer hover:text-gray-400">Testimonials</li>
-        </ul>
-      </div>
+      )}
     </div>
   );
 };
