4b8335e258b552941dff464fb0ef8d21225a595a
venky-webdev-pro <Nimmanavenkatalakshmi93@gmail.com>
2025-07-13 18:01:37 +0530

    ProjectsUpdate

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
-import { assets, projectsData } from '../assets/assets';
+import React, { useEffect, useState } from "react";
+import { assets, projectsData } from "../assets/assets";
 
 const Projects = () => {
+    const [currentIndex,setCurrentIndex] = useState(0);
+    const [cardsToShow,setCardsToShow] = useState(1);
+
+    const nextButton = () => {          
+        setCurrentIndex((prevIndex)=>(prevIndex+1) % projectsData.length)
+    }
+
+    const prevButton = () =>{
+        setCurrentIndex((prevIndex) => prevIndex === 0 ? projectsData.length - 1 : prevIndex - 1)
+    }
+
+    useEffect(()=>{
+        const updateCardsToShow = () =>{
+            if(window.innerWidth >= 1024){
+                setCardsToShow(projectsData.length);
+            }else{
+                setCardsToShow(1)
+            }
+        };
+        updateCardsToShow();
+
+        window.addEventListener('resize',updateCardsToShow);
+        return () => window.removeEventListener('resize',updateCardsToShow);
+    },[])
   return (
-<div className="mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden">
+    <div className="mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden">
       <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center">
         Projects{" "}
         <span className="underline underline-offset-4 decoration-1 font-light">
@@ -15,36 +39,38 @@ const Projects = () => {
       </p>
       {/* slider buttons */}
       <div className="flex justify-end items-center mb-8">
-        <button  className="p-3  bg-gray-200 rounded mr-2 cursor-pointer" aria-label="Previous Project">
+        <button onClick={prevButton} className="p-3  bg-gray-200 rounded mr-2 cursor-pointer" aria-label="Previous Project">
           <img src={assets.left_arrow} alt="Previous" />
         </button>
-         <button  className="p-3 bg-gray-200 rounded mr-2 cursor-pointer" aria-label="Next Project">
+         <button onClick={nextButton} className="p-3 bg-gray-200 rounded mr-2 cursor-pointer" aria-label="Next Project">
           <img src={assets.right_arrow} alt="Next" />
         </button>
       </div>
-      {/* projets slides container  */}
-      <div>
-        <div>
-            <div className='flex gap-8 transition-transform  duration-500 ease-in-out'>
-  {projectsData?.map((Project, index) => (
-    <div key={index}  className='w-full shrink-0 sm:w-1/4  relative'>
-      <img  className="w-full h-auto mb-14"src={Project?.image} alt={Project.title} />
-      <div className='flex justify-center absolute left-0 bottom-5 right-0 '>
-      <div className='inline-block bg-white w-3/4 px-4 py-2 shadow-md'> 
-        <h2 className='text-xl font-semibold text-gray-800'>{Project.title}</h2>
-        <p className='text-gray-500 text-sm'>{Project.price}<span className='px-1'>|{ Project.location} </span></p>
-</div>
-      </div>
-    </div>
-  ))}
-</div>
+
+        {/* project slider container */}
+        <div className="overflow-hidden">
+            <div className="flex gap-8 transition-transform duration-500 ease-in-out" style={{transform:`translateX(-${(currentIndex*100)/cardsToShow}%)`}}>  
+                { projectsData.length>0 && projectsData.map((project,index)=>( 
+                  
+                    <div key={index} className="relative w-full shrink-0 sm:w-1/4">
+                        <img src={project.image} alt={project.title} className="w-full h-auto mb-14"/>
+                        <div className="absolute left-0 right-0 bottom-5 flex justify-center">
+                            <div className="inline-block bg-white w-3/4 px-4 py-2 shadow-md text-center">
+                                <h2 className="text-xl font-semibold text-gray-800">
+                                    {project.title}
+                                </h2>
+                                <p className="text-gray-500 text-sm">
+                                    {project.price} <span className="px-1"> | </span> {project.location}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
-      </div>
 
-
-
-</div>
-  )
-}
+    </div>
+  );
+};
 
 export default Projects;
